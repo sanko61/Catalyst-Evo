3374431601b611f12711b7fb51a41fbfffb61c35
root <root@nur1abs.nur1labshq.net>
2018-06-20 09:51:00 +0900

    updates time block

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -94,7 +94,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "catalyst";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001c5f5b7d9a43a029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101b7b5bf9a5ef060d691b46f04e013f794ecf2c5b5b598036cbfc420f8ecc7e291";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001c5f5b7d9a43a029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f72608d46fad95a40a78e8b766e19465b12b211fb0215809d17e9ee6136d1d88";
 
 const uint32_t GENESIS_NONCE                                 = 70;
 const uint64_t GENESIS_TIMESTAMP                             = 1529201466;
