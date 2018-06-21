575643b5bc27280f286bac96124aa3769602f569
root <root@nur1abs.nur1labshq.net>
2018-06-21 18:51:18 +0900

    core

--- src/base/CryptoNoteFormatUtils.cpp
+++ src/base/CryptoNoteFormatUtils.cpp
@@ -501,8 +501,7 @@ bool get_block_longhash(cn_context &context, const Block& b, Hash& res) {
   } else {
     return false;
   }
-  const int cn_variant = b.majorVersion >= 3 ? b.majorVersion - 2 : 0;
-  cn_slow_hash(context, bd.data(), bd.size(), res, cn_variant);
+  cn_slow_hash(context, bd.data(), bd.size(), res, b.majorVersion);
   return true;
 }
 //------------------------------------------------------------- Seperator Code -------------------------------------------------------------//
